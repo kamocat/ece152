4549904746e648e93510089a50c98b8e62226619
Marshal Horn <kamocat@gmail.com>
2012-04-28 18:33:02 -0700

    led.h now includes util/delay.h, which it is dependent on

--- lib/led.h
+++ lib/led.h
@@ -9,6 +9,11 @@
 #define AMBER 0b11000000
 #endif
 
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+#include <util/delay.h>
+
 #ifndef HORNM_LED_DISPLAY_COLUMN
 #define HORNM_LED_DISPLAY_COLUMN
 /*
